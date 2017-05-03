3bbc29480bf374de0c611e386d8a74a72d6a59c3
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-05-03 11:43:59 -0700

    Create additional framework for front-end
    - Change .gitignore to stop ignoring files in /dist folder
    - Create a webpack.config.js file (pulled from mvp-starter)
    - Create index.html which is the initial 'homepage'
    - Create index.jsx which will contain the master 'app' component
    - Add jquery to list of depdencies in package.json

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+// import TodoList from './components/TodoList.jsx';
+// import AddTodo from './components/AddTodo.jsx';
