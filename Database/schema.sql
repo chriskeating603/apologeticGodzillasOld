caff549a93ca4575af9e3ff0d21b33104742e525
Chris Keating <ckeating.nh@gmail.com>
2017-05-03 11:46:38 -0700

    Adds mySQL database setup

---
+++ Database/schema.sql
@@ -0,0 +1,19 @@
+drop database if exists valence;
+
+create valence;
+
+use valence;
+
+create table users (
+  id int primary key auto_increment,
+  username varchar unique
+);
+
+create table entries (
+  id int primary key auto_increment,
+  sleepHrs int(2),
+  minsExercise int(2),
+  mood int(2),
+  users_id int(2),
+  foreign key users_id references users(id)
+);
